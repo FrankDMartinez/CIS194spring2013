d1039fbb9b8be705e5d33224292293d633d752a0
FrankDMartinez <Frank@tests-Mac-mini.local>
2015-01-06 16:50:19 -0500

    lesson 1, exercise 2

--- lesson1/Main.hs
+++ lesson1/Main.hs
@@ -1,10 +1,23 @@
 -- convert positive `Integers` into a list of digits; for 0 or
 -- negative inputs, `toDigits` should return an empty list
 toDigits    :: Integer -> [Integer]
-toDigits n
-  | n <= 0  = []
-  | otherwise = toDigits (n `div`10) ++ [n `mod` 10]
+toDigits a
+  | a <= 0  = []
+  | otherwise = toDigits (a `div`10) ++ [a `mod` 10]
 
 -- the same as `toDigits` but with the digits reversed
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev n = reverse (toDigits n)
+toDigitsRev a = reverse (toDigits a)
+
+-- doubles every other number beginning from the right (e.g.,
+-- the "second-to-last", "fourth-to-last", ... numbers are
+-- doubled)
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther a = reverse (doubleEveryOtherHelper (reverse a))
+
+-- doubles every other number beginning from the left (e.g., the
+-- "second", "fourth", ... numbers are doubled)
+doubleEveryOtherHelper :: [Integer] -> [Integer]
+doubleEveryOtherHelper []         = []
+doubleEveryOtherHelper (a:[])        = [a]
+doubleEveryOtherHelper (a:(b:cs)) = (a) : (2*b) : (doubleEveryOtherHelper cs)
